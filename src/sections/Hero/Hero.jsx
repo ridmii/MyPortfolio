bb28925bde48ca1a67a0a4ec0f2c43ff802eb30e
Ridmi Vancuylenburg <heyridmi@gmail.com>
2024-11-16 12:15:44 +0530

    Initial commit

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,7 +8,7 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/My Resume.pdf';
+import CV from '../../assets/My Resume (2).pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
