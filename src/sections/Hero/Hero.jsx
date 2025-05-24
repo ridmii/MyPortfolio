018267572e6fe6532a1bd0767a923449650e86a8
Ridmi Vancuylenburg <heyridmi@gmail.com>
2025-05-24 13:56:14 +0530

    resume updated

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -9,7 +9,7 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/Resume.pdf';
+import CV from '../../assets/R.M. Vancuylenburg.pdf';
 import { useTheme } from '../../common/ThemeContext';
 import developerAnimation from '../../assets/hh.json';
 
